44797873e07bb0d3fbfb522487d1bc9690e90bd4
ZKW0001 <2655053043@qq.com>
2024-11-24 02:40:16 +0000

    code update

---
+++ EEEN30111-Lab1-New/main.d
@@ -0,0 +1,41 @@
+eeen30111-lab1-new/main.o: ..\Core\Src\main.c ..\Core\Inc\main.h \
+  ..\Drivers\STM32F4xx_HAL_Driver\Inc\stm32f4xx_ll_i2c.h \
+  ..\Drivers\CMSIS\Device\ST\STM32F4xx\Include\stm32f4xx.h \
+  ..\Drivers\CMSIS\Device\ST\STM32F4xx\Include\stm32f401xe.h \
+  ..\Drivers\CMSIS\Include\core_cm4.h \
+  ..\Drivers\CMSIS\Device\ST\STM32F4xx\Include\system_stm32f4xx.h \
+  ..\Drivers\STM32F4xx_HAL_Driver\Inc\stm32f4xx_ll_rcc.h \
+  ..\Drivers\STM32F4xx_HAL_Driver\Inc\stm32f4xx_ll_bus.h \
+  ..\Drivers\STM32F4xx_HAL_Driver\Inc\stm32f4xx_ll_system.h \
+  ..\Drivers\STM32F4xx_HAL_Driver\Inc\stm32f4xx_ll_exti.h \
+  ..\Drivers\STM32F4xx_HAL_Driver\Inc\stm32f4xx_ll_cortex.h \
+  ..\Drivers\STM32F4xx_HAL_Driver\Inc\stm32f4xx_ll_utils.h \
+  ..\Drivers\STM32F4xx_HAL_Driver\Inc\stm32f4xx_ll_pwr.h \
+  ..\Drivers\STM32F4xx_HAL_Driver\Inc\stm32f4xx_ll_dma.h \
+  ..\Drivers\STM32F4xx_HAL_Driver\Inc\stm32f4xx_ll_spi.h \
+  ..\Drivers\STM32F4xx_HAL_Driver\Inc\stm32f4xx_ll_usart.h \
+  ..\Drivers\STM32F4xx_HAL_Driver\Inc\stm32f4xx_ll_gpio.h \
+  ..\Core\Inc\spi.h ..\Core\Inc\gpio.h ..\Core\Inc\i2c.h \
+  ..\Core\Inc\lcd.h ..\Core\Inc\fonts\Fonts.h \
+  ..\Core\Inc\fonts\Arial6x9.h ..\Core\Inc\fonts\Arial8x8.h \
+  ..\Core\Inc\fonts\Arial9x9.h ..\Core\Inc\fonts\Arial11x11.h \
+  ..\Core\Inc\fonts\Arial12x12.h ..\Core\Inc\fonts\Arial24x23i.h \
+  ..\Core\Inc\fonts\Arial28x28.h ..\Core\Inc\fonts\ArialR8x8.h \
+  ..\Core\Inc\fonts\ArialR12x14.h ..\Core\Inc\fonts\ArialR16x17.h \
+  ..\Core\Inc\fonts\ArialR20x20.h ..\Core\Inc\fonts\ArialR32x34.h \
+  ..\Core\Inc\fonts\Computerfont11x15.h \
+  ..\Core\Inc\fonts\CooperBlack21x18.h ..\Core\Inc\fonts\Curlz38x48.h \
+  ..\Core\Inc\fonts\Digital11x17.h ..\Core\Inc\fonts\Digital16x31.h \
+  ..\Core\Inc\fonts\Digital22x38.h ..\Core\Inc\fonts\Digital25x31.h \
+  ..\Core\Inc\fonts\Fixedsys8x14.h ..\Core\Inc\fonts\Fixedsys9x14B.h \
+  ..\Core\Inc\fonts\LCD8x13.h ..\Core\Inc\fonts\LCD8x14.h \
+  ..\Core\Inc\fonts\LCD14x23.h ..\Core\Inc\fonts\LCD19x29.h \
+  ..\Core\Inc\fonts\LCD28x42.h ..\Core\Inc\fonts\LCD36x56.h \
+  ..\Core\Inc\fonts\LED41x41.h ..\Core\Inc\fonts\Neu31x26.h \
+  ..\Core\Inc\fonts\Neu42x35.h ..\Core\Inc\fonts\Neu44x36.h \
+  ..\Core\Inc\fonts\OldEnglish20x19.h \
+  ..\Core\Inc\fonts\OldEnglish33x33.h ..\Core\Inc\fonts\Radius22x24.h \
+  ..\Core\Inc\fonts\SCProSB31x55.h ..\Core\Inc\fonts\SmallFont7x6.h \
+  ..\Core\Inc\fonts\TimesNR13x15.h ..\Core\Inc\fonts\TimesNR16x16.h \
+  ..\Core\Inc\fonts\TimesNR19x18.h ..\Core\Inc\fonts\TimesNR28x25.h \
+  ..\Core\Inc\fonts\TimesNR39x37.h
